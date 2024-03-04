55fd235efad09d163b4b879e30be08995bc343da
ar1ocker <ar1oc@yandex.ru>
2024-03-04 10:42:53 +0300

    Удаление остатков мертвого кода

--- global_checks.py
+++
@@ -1,9 +0,0 @@
-from discord.ext import commands
-
-
-async def block_direct_message(context: commands.Context) -> bool:
-    return context.guild is not None
-
-
-def register_global_checks(bot: commands.Bot) -> None:
-    bot.add_check(block_direct_message)
